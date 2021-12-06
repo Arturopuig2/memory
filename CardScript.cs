18c146294f0883db88a5d583688d48c7a11bfcc4
Arturo Puig <arturopuig2@gmail.com>
2021-12-07 00:15:42 +0100

    Primer commit Memoriy

---
+++ CardScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+
+    public Sprite imagen;
+    //public Vector3 posicion;
+    //public Vector3 rotacion;
+
+    //    public Sprite ImagenCarta()
+    //{
+    //    return imagen;
+    //}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = imagen;
+        //posicion = transform.position;
+        //Debug.Log(posicion);
+        //posicion = new Vector3(-6,1,0);
+        //transform.Translate(posicion2);
+        //rotacion = new Vector3(0, 0, 90)*Time.deltaTime;
+        //transform.Rotate(rotacion);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //    Debug.Log("Pressed primary button.");
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("He hecho clic en la carta "+name);
+    }
+
+
+}
---
+++ GameManagerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public GameObject MyPrefab;
+
+    public List<GameObject> ListaCartas = new List<GameObject>();
+    public int filas = 2;
+    public int columnas = 5;
+    public int columnasAsignadas = 0;
+    public int filasAsignadas = 0;
+
+    public void ColocarCartas()
+    {
+        float posX = -6;
+        float posY = 3.2f;
+
+        for (int i = 0; i < filas * columnas; i++)
+        {
+            GameObject nueva_carta = Instantiate(MyPrefab, new Vector3(posX,posY, 0), Quaternion.identity);
+            nueva_carta.name = "Carta" + i;
+            ListaCartas.Add(nueva_carta);
+            posX += 3;
+            columnasAsignadas+=1;
+
+            if (columnasAsignadas == columnas)
+            {
+                posX = -6;
+                posY = posY-3.2f;
+                columnasAsignadas = 0;
+                filasAsignadas += 1;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ColocarCartas();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
